50b3f7c894ccd688ea847be5426d310cb92b9a8c
Giadaspt <saputo.giada@gmail.com>
2021-10-21 13:53:31 +0200

    primo commit biglietto del treno

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//calcolo del prezzo del biglietto del treno
+
+/*
+1. chiedo all'utente i km che deve percorrere e l'età
+2. calcolare il prezzo del biglietto sapendo che ogni km sono 0.21 euro, quindi moltiplicarlo per il totale dei km del passeggero
+3. se il passeggero è minorenne ha il 20% di sconto sul totale del biglietto
+4. se il passeggero ha più di 65 anni viene applicato il 40% sul costo totale del biglietto
+5. l'output finale è il costo del biglietto compresi i centesimi 
+*/ 
